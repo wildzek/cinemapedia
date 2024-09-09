fdf976029d15fa09f219585a42efdce9928f8785
wildzek <dev@rogertorrent.com>
2024-09-09 20:58:57 +0200

    Initial app_theme configuration

---
+++ cinemapedia/lib/config/theme/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getTheme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: const Color.fromARGB(255, 70, 9, 131));
+}
