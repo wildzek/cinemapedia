14ae45a6336399a66ec3c2f7544da09ded9cf1f1
wildzek <dev@rogertorrent.com>
2024-09-24 22:48:42 +0200

    Environment variables configured to home_screen, environment class created

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,5 @@
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(
-      child: Text(dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay api key'),
+      child: Text(Environment.theMovieDbKey),
     ));
   }
 }
