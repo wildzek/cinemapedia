48b49e97aeb17b4ecf0f384d93f76e2dcbaa208a
wildzek <dev@rogertorrent.com>
2024-10-08 20:52:35 +0200

    Functionality added to the provider to enable the notifier

--- cinemapedia/lib/presentation/providers/movies/movies_providers.dart
+++ cinemapedia/lib/presentation/providers/movies/movies_providers.dart
@@ -1,16 +1,27 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final nowPlayingMoviesProvider =
     StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
-  return MoviesNotifier();
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPLaying;
+  return MoviesNotifier(
+    fecthMoreMovies: fetchMoreMovies
+  );
 });
+
+typedef MovieCallback = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   int currentPage = 0;
-  MoviesNotifier() : super([]);
+  MovieCallback fecthMoreMovies;
 
-  Future<void> loadNextPage() async{
-    
+  MoviesNotifier({required this.fecthMoreMovies}) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+
+    final List<Movie> movies = await fecthMoreMovies(page: currentPage);
+    state = [...state, ...movies];
   }
 }
