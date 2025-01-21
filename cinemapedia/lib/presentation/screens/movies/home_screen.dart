8d9eee10613308145d1b2f1860cdfd56001d3aec
wildzek <dev@rogertorrent.com>
2025-01-21 21:19:54 +0100

    Circular progress indicator added

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -31,6 +31,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
+    if (nowPlayingMovies.length == 0) return CircularProgressIndicator();
     return ListView.builder(
         itemCount: nowPlayingMovies.length,
         itemBuilder: (context, index) {
