fa0e7e8ae947b24ff09ff2553d7ea6c9a48d2798
wildzek <dev@rogertorrent.com>
2025-01-23 18:50:12 +0100

    Movie slideshow provider enabled to only receive 6 films on the slideshow

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -32,13 +30,14 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    if (nowPlayingMovies.length == 0) return CircularProgressIndicator();
+    //if (nowPlayingMovies.length == 0) return CircularProgressIndicator();
+
+    final moviesSideshow = ref.watch(moviesSlideshowProvider);
     return Column(children: [
-
       const CustomAppbar(),
-      MoviesSlideshow(movies: nowPlayingMovies)
+      MoviesSlideshow(movies: moviesSideshow)
     ]);
   }
 }
