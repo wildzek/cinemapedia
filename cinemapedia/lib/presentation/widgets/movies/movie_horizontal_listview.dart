ba18cea35d3a6105091b2e455a98301e0f54ea94
wildzek <dev@rogertorrent.com>
2025-01-28 21:15:51 +0100

    Movies added to the new listview, also configured text and ratings

--- cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -21,9 +22,77 @@ class MovieHorizontalListview extends StatelessWidget {
       child: Column(
         children: [
           if (title != null || subtitle != null)
-            _Title(title: title, subtitle: subtitle)
+            _Title(title: title, subtitle: subtitle),
+          Expanded(
+              child: ListView.builder(
+            itemCount: movies.length,
+            scrollDirection: Axis.horizontal,
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return _Slide(movie: movies[index]);
+            },
+          ))
         ],
       ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      //*Image
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        SizedBox(
+          width: 150,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              movie.posterPath,
+              fit: BoxFit.cover,
+              width: 150,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress != null) {
+                  return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                          child: CircularProgressIndicator(strokeWidth: 2)));
+                }
+                return FadeIn(child: child);
+              },
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 5),
+        //*Text
+        SizedBox(
+          width: 150,
+          child: Text(
+            movie.title, 
+            maxLines: 2,
+            style: textStyles.titleSmall
+          )
+        ),
+
+        //*Rating
+        Row(children: [
+          Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+          const SizedBox(width: 3),
+          Text('${movie.voteAverage}', style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
+          const SizedBox(width: 10),
+          Text('${movie.popularity}', style: textStyles.bodySmall)
+        ])
+
+
+
+      ]),
     );
   }
 }
@@ -40,23 +109,14 @@ class _Title extends StatelessWidget {
     return Container(
         padding: const EdgeInsets.only(top: 10),
         margin: const EdgeInsets.symmetric(horizontal: 10),
-        child: Row(
-            children: [
-              if (title != null) 
-              Text(title!, style: titleStyle),
-
-              const Spacer(),
-
-              if (subtitle != null) 
-              FilledButton.tonal(
+        child: Row(children: [
+          if (title != null) Text(title!, style: titleStyle),
+          const Spacer(),
+          if (subtitle != null)
+            FilledButton.tonal(
                 style: const ButtonStyle(visualDensity: VisualDensity.compact),
-                onPressed: (){}, 
-                child: 
-                  Text(subtitle!)
-              ),
-            
-            ]
-        )
-    );
+                onPressed: () {},
+                child: Text(subtitle!)),
+        ]));
   }
 }
