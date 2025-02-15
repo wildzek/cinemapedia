116588eeaed32430d2d7d30d48a771ef8e2fd52e
wildzek <dev@rogertorrent.com>
2025-02-15 13:20:21 +0100

    CustomScrollView implemented on the home_screen

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -38,16 +38,51 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     //if (nowPlayingMovies.length == 0) return CircularProgressIndicator();
 
     final moviesSideshow = ref.watch(moviesSlideshowProvider);
-    return Column(children: [
-      const CustomAppbar(),
-      MoviesSlideshow(movies: moviesSideshow),
-      MovieHorizontalListview(
-          movies: nowPlayingMovies,
-          title: 'En cines',
-          subtitle: 'Lunes 20',
-          loadNextPage: () {
-            ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-          })
+    return CustomScrollView(
+      slivers: [
+
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: CustomAppbar(),
+          ),
+        ),
+        SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+          return Column(children: [
+            //const CustomAppbar(),
+            MoviesSlideshow(movies: moviesSideshow),
+            MovieHorizontalListview(
+                movies: nowPlayingMovies,
+                title: 'En cines',
+                subtitle: 'Lunes 20',
+                loadNextPage: () {
+                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+                }),
+            MovieHorizontalListview(
+                movies: nowPlayingMovies,
+                title: 'Próximamente',
+                subtitle: 'En este mes',
+                loadNextPage: () {
+                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+                }),
+            MovieHorizontalListview(
+                movies: nowPlayingMovies,
+                title: 'Populares',
+                subtitle: 'Tendencias',
+                loadNextPage: () {
+                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+                }),
+            MovieHorizontalListview(
+                movies: nowPlayingMovies,
+                title: 'Mejor calificadas',
+                subtitle: 'Desde siempre',
+                loadNextPage: () {
+                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+                }),
+            const SizedBox(height: 10)
+          ]);
+        }, childCount: 1)),
     ]);
   }
 }
