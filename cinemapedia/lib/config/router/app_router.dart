9ae6c19d41c29a860d7e169215b8d8f781d4d7df
wildzek <dev@rogertorrent.com>
2025-02-26 21:37:19 +0100

    New page defined in go router

--- cinemapedia/lib/config/router/app_router.dart
+++ cinemapedia/lib/config/router/app_router.dart
@@ -1,14 +1,18 @@
 import 'package:cinemapedia/presentation/screens/movies/home_screen.dart';
+import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-    )
-
-  ]);
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen(),
+  ),
+  GoRoute(
+      path: '/movie/:id',
+      name: MovieScreen.name,
+      builder: (context, state) {
+        final movieId = state.pathParameters['id'] ?? 'no-id';
+        return MovieScreen(movieId: movieId);
+      })
+]);
--- cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -7,6 +7,10 @@ class MovieScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MovieID: $movieId'),
+      ),
+    );
   }
 }
