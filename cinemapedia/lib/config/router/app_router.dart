def2997d7fa81ea82f994afebf82189aee22548c
wildzek <dev@rogertorrent.com>
2025-02-26 21:47:03 +0100

    Navigation to the new movie screen configured

--- cinemapedia/lib/config/router/app_router.dart
+++ cinemapedia/lib/config/router/app_router.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia/presentation/screens/movies/home_screen.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
@@ -7,12 +6,15 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
     path: '/',
     name: HomeScreen.name,
     builder: (context, state) => const HomeScreen(),
+    routes: [
+      GoRoute(
+        path: 'movie/:id',
+        name: MovieScreen.name,
+        builder: (context, state) {
+          final movieId = state.pathParameters['id'] ?? 'no-id';
+          return MovieScreen(movieId: movieId);
+        })
+    ]
   ),
-  GoRoute(
-      path: '/movie/:id',
-      name: MovieScreen.name,
-      builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? 'no-id';
-        return MovieScreen(movieId: movieId);
-      })
+
 ]);
